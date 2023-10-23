5bbbd31d6cd60e19b7c2cecd6a6a99cf89eafc82
Uyen <uyenntt71@viettel.com.vn>
2023-10-23 19:59:42 +0700

    add HLS player

--- src/app/vod/page.tsx
+++ src/app/vod/page.tsx
@@ -1,14 +1,70 @@
-import { Box } from "@chakra-ui/react";
+'use client';
+
+import VideoPlayer from '@/components/VideoPlayer';
+import { HLS_TYPES, SOURCE_TYPES, VOD_OPTIONS } from '@/utils/constant';
+import { Box, Grid, GridItem, Input, Select, Textarea } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 export default function Vod() {
+  const [selectedUrl, setSelectedUrl] = useState<string>('');
+  const [isLowLatency, setIsLowLatency] = useState<boolean>(false);
+
+  const handleSetHlsType = (hlsType: string) => {
+    setIsLowLatency(hlsType === 'hlsll');
+  };
+
+  const handleSetUrl = (key: number) => {
+    const selectedVod = VOD_OPTIONS.filter((item) => item.key == key)?.[0];
+    setSelectedUrl(isLowLatency ? selectedVod?.hlsllUrl : selectedVod?.hlsUrl);
+  };
+
   return (
-    <Box p="2" h="88vh" w="100%">
-      <Grid
-      border={'md'}
-      templateRows={`repeat(${rowSpan}, 1fr)`}
-      w='full'
-      h='full'
-    >
-    </Box>
+    <>
+      <title>Video-on-demand player</title>
+      <Box p="5" h="100vh" w="100%">
+        <Grid border={'md'} templateRows={`repeat(20, 1fr)`} w="full" h="full">
+          <GridItem
+            rowSpan={1}
+            display={'flex'}
+            justifyContent={'space-between'}
+            shadow={'md'}
+          >
+            <Select
+              placeholder="Select HLS Type"
+              onChange={(e: any) => handleSetHlsType(e.target.value)}
+              defaultValue={HLS_TYPES[0].key}
+            >
+              {HLS_TYPES.map((item) => {
+                return <option value={item.key}>{item.value}</option>;
+              })}
+            </Select>
+            <Select
+              placeholder="Select Video"
+              onChange={(e: any) => handleSetUrl(e.target.value)}
+              defaultValue={VOD_OPTIONS[0].hlsUrl}
+            >
+              {VOD_OPTIONS.map((item) => {
+                return <option value={item.key}>{item.name}</option>;
+              })}
+            </Select>
+          </GridItem>
+          <GridItem
+            rowSpan={19}
+            display={'flex'}
+            justifyContent={'space-between'}
+            shadow={'md'}
+          >
+            <Box w={'1280px'} h={'720px'}>
+              {selectedUrl && (
+                <VideoPlayer
+                  videoSource={selectedUrl}
+                  type={SOURCE_TYPES.MPEGURL}
+                />
+              )}
+            </Box>
+          </GridItem>
+        </Grid>
+      </Box>
+    </>
   );
 }
---
+++ src/utils/constant.ts
@@ -0,0 +1,52 @@
+export type VodData = {
+  key: number;
+  name: string;
+  hlsUrl: string;
+  hlsllUrl: string;
+};
+
+export const VOD_OPTIONS: Array<VodData> = [
+  {
+    key: 1,
+    name: 'FDR - CDN packaged, 4s segments, 180p - 1080p',
+    hlsUrl: 'https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8',
+    hlsllUrl: 'https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8',
+  },
+  {
+    key: 2,
+    name: 'CNN special report, with CC',
+    hlsUrl:
+      'https://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8',
+    hlsllUrl:
+      'https://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8',
+  },
+  {
+    key: 3,
+    name: 'Redundant levels with subtitle and audio track groups',
+    hlsUrl:
+      'https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/redundant.m3u8',
+    hlsllUrl:
+      'https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/redundant.m3u8',
+  },
+];
+
+export enum SOURCE_TYPES {
+  MPEGURL = 'application/x-mpegURL',
+  MP4 = 'video/mp4',
+}
+
+export type HlsType = {
+  key: string;
+  value: string;
+};
+
+export const HLS_TYPES: Array<HlsType> = [
+  {
+    key: 'hls',
+    value: 'HLS',
+  },
+  {
+    key: 'hlsll',
+    value: 'HLS Low Latency',
+  },
+];
